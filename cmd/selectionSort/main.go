1a698e23db286bd095526fd240419cd5595a97ca
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-11-25 11:24:23 +0330

    fix the issue of the selection sort

--- cmd/selectionSort/main.go
+++ cmd/selectionSort/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	nums := []int{88, 23, 15, 70, 33}
+	nums := []int{9, 4, 6, 1, 2, 8}
 	fmt.Println(nums)
 	fmt.Println(selectionSort.SelectionSort(nums))
 }
--- selectionSort/selectionSort.go
+++ selectionSort/selectionSort.go
@@ -5,10 +5,12 @@ func SelectionSort(arr []int) []int {
 	for i := 0; i < n-1; i++ {
 		minIndex := i
 		for j := i + 1; j < n; j++ {
-			if arr[minIndex] < arr[j] {
+			if arr[j] < arr[minIndex] {
 				minIndex = j
 			}
-			arr[j], arr[minIndex] = arr[minIndex], arr[j]
+		}
+		if minIndex != i {
+			arr[i], arr[minIndex] = arr[minIndex], arr[i]
 		}
 	}
 	return arr
