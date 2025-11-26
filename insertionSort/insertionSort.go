c7410be2df4e92669807040c7173c852d3b2b1e0
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-11-26 12:25:49 +0330

    make improvements to the insertion sort

--- insertionSort/insertionSort.go
+++ insertionSort/insertionSort.go
@@ -2,13 +2,13 @@ package insertionSort
 
 func InsertionSort(arr []int) []int {
 	for i := 1; i < len(arr); i++ {
-		key := arr[i]
 		j := i - 1
-		for j >= 0 && arr[j] > key {
+		for j >= 0 && arr[j+1] < arr[j] {
+			tmp := arr[j+1]
 			arr[j+1] = arr[j]
+			arr[j] = tmp
 			j--
 		}
-		arr[j+1] = key
 	}
 	return arr
 }
